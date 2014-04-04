f63f381fa09179e3446b7c1e377e9c66f17d4c09
Agych <alexander_agych@mail.ru>
2014-04-04 14:05:59 +0200

    Game Project Prototype Upload

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace WindowsGame1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
